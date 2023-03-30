3a3b6d68e2623a35700fe9e6a0bda8a2397d9381
madeleinehll <hill.madeleine13@yahoo.de>
2023-03-30 15:52:46 +0200

    title und zoom

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let stop_lat = -36.833333;
 let stop_lng = 174.8;
+let zoom = 13;
+let title = 'Auckland'
 
-let map = L.map('map').setView([stop_lat, stop_lng], 12);
+let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([stop_lat, stop_lng]).addTo(map)
-    .bindPopup('Auckland.')
+    .bindPopup(title)
     .openPopup();
