786426192f80921d324bc00af11ebb68409aa908
madeleinehll <hill.madeleine13@yahoo.de>
2023-03-30 15:46:04 +0200

    variablen setzen

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-var map = L.map('map').setView([-36.833333, 174.8], 12);
+let stop_lat = -36.833333;
+let stop_lng = 174.8;
+
+let map = L.map('map').setView([stop_lat, stop_lng], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-36.833333, 174.8]).addTo(map)
+L.marker([stop_lat, stop_lng]).addTo(map)
     .bindPopup('Auckland.')
     .openPopup();
