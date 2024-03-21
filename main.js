dbf63a800353184217985f73af5ae8f3db65a6bb
madeleinehll <hill.madeleine13@yahoo.de>
2024-03-21 17:39:43 +0100

    script weg

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-<script>
 var map = L.map('map').setView([-37.8575, 175.679722], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -25,4 +24,3 @@ function onMapClick(e) {
 }
 
 map.on('click', onMapClick);
-</script>
